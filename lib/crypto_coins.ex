30a93594f759252402e68091c6e59702e38760ed
Jorge Ochoa <jochoa2@ucol.mx>
2019-12-03 09:50:06 -0600

    Finish Api

--- lib/crypto_coins.ex
+++ lib/crypto_coins.ex
@@ -1,16 +1,23 @@
+require CryptoCoins.Api
 defmodule CryptoCoins do
+
   def coin(coins, name) when is_binary(name) do
+    Enum.find(coins, fn coin -> Map.fetch!(coin, "name") == name end)
   end
 
   def most_valuable(coins) do
+    Enum.max_by(coins, fn coin -> Float.parse(Map.fetch!(coin, "price"))end)
   end
 
   def most_capped(coins) do
+    Enum.max_by(coins, fn coin -> Map.fetch!(coin, "marketCap")end)
   end
 
   def most_ranked(coins) do
+    Enum.min_by(coins, fn coin -> Map.fetch!(coin, "rank")end)
   end
 
   def sort_by(coins, field) do
+    Enum.sort_by(coins, fn coin -> Map.fetch!(coin, field) == field end)
   end
 end
--- lib/crypto_coins/api.ex
+++ lib/crypto_coins/api.ex
@@ -1,14 +1,31 @@
-defmodule Api.CryptoCoins do
+defmodule CryptoCoins.Api do
   # Check Tesla Docs
   # API Base is: "https://api.coinranking.com/v1/public"
   # Endpoint is: /coins
 
+    use Tesla
+
+    plug Tesla.Middleware.BaseUrl, "https://api.coinranking.com/v1/public"
+    plug Tesla.Middleware.Headers, [{"", ""}]
+    plug Tesla.Middleware.JSON
+
+
+
   # returns {:ok, array_of_coins}
   def coins() do
+    {:ok, response} = get("/coins")
+    coins = response |> Map.fetch!(:body) |> Map.fetch!("data") |> Map.fetch!("coins")
   end
 
   # returns array_of_coins
   #raise if error
   def coins!() do
+    {:ok, response} = get("/coins")
+
+    if(response.status in 200..299) do
+      coins = response |> Map.fetch!(:body) |> Map.fetch!("data") |> Map.fetch!("coins")
+    else
+      raise "Adios!"
+    end
   end
 end
